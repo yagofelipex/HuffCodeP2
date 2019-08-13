76d35212531f66434fc494c2262568301dbfcdea
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-08-13 18:19:31 -0300

    Criação do arquivo Huffman para iniciar o codigo.

---
+++ Huffman/Huffman.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct HuffTree
+{
+	unsigned int frequencia;
+	void* valor;
+	struct HuffTree *esq;
+	struct HuffTree *dir;
+};
+
+struct HuffTree init()
+{
+	struct HuffTree novo_valor = (struct Huff*)malloc(sizeof(struct Huff));
+	novo_valor->valor = NULL;
+	novo_valor->frequencia = NULL;
+	novo_valor->esq = NULL;
+	novo_valor->dir =  NULL;
+	return novo_valor;
+}
+
+struct HuffTree add(unsigned int frequencia, void* valor, struct HuffTree *esq, struct HuffTree *dir)
+{
+	struct HuffTree novo_valor = (struct Huff*)malloc(sizeof(struct Huff));
+	novo_valor->valor = valor;
+	novo_valor->frequencia = frequencia;
+	novo_valor->esq = esq;
+	novo_valor->dir =  dir;
+	return novo_valor;
+}
+
+unsigned char eh_folha(struct HuffTree huffman)
+{
+	if((huffman->esq == NULL) && (huffman->dir == NULL))
+		return 1;
+	else
+		return 0;
+}
+
+int main() {
+
+	return 0;
+}
