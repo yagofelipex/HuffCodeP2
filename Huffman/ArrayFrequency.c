897982645ba2d8289f3de942049fd951b5158807
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-08-14 22:50:43 -0300

    Edição em todo o codigo

--- Huffman/ArrayFrequency.c
+++ Huffman/ArrayFrequency.c
@@ -1,68 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Data_character
+/*typedef struct Data_character
 {
 	char character[1000];
 	int frequency;
-}Creat;
-void Print(int N_repeated,Creat *ArrayFrequency)
+}Creat;*/
+
+void zerar_array_frequencia(char array_frequencia[])
 {
 	int i;
-	for (i = 0;i < N_repeated;i++)
+	for(i = 0; i < 256; i++)
 	{
-		printf("%c %d\n",ArrayFrequency[i].character[i],ArrayFrequency[i].frequency);
+		array_frequencia[i] = 0;
 	}
 }
-int  Comparison(int index,int i,int j,int count,int size,char string[],Creat ArrayFrequency[])
+int main()
 {
-    if (i == size || index == size)
-    {
-        return j;
-    }
+    FILE *file_input; //variavel que guardará o arquivo de entrada
+    unsigned char caracter;
+    char nome_arquivo[30];
+	unsigned char string[256];
+    int i = 0;
+
+    zerar_array_frequencia(string);
+
+    scanf("%s", nome_arquivo);
+    file_input = fopen(nome_arquivo, "rb");
+
+    if(file_input == NULL)
+	{
+		printf("ERRO AO ABRIR ARQUIVO: %s\n", nome_arquivo);
+		return 0;
+	}
     else
     {
-        if (string[i] == string[index])
-        {
-            count++;
-            ++i;
-        }
-        if (string[i] != string[index])
-        {
-        ArrayFrequency[j].character[j] = string[index];
-        ArrayFrequency[j].frequency = count;
-        ++j;
-        count = 0;
-        index = i;
-        }
+    	while(fscanf(file_input, "%c", &caracter) != EOF)
+		{
+			printf("%c", caracter);
+			string[caracter]++;
+		}
     }
-     Comparison(index,i,j,count,size,string,ArrayFrequency);
-}
-void Bubble_sort(int i,int size,char *string)
-{
-if (i < size - 1)
-{
-if (string[i] > string[i + 1])
-{
-char aux            = string[i];
-string[i]           = string[i + 1];
-string[i + 1]       = aux;
-}
-Bubble_sort(++i,size,string);
-}
-else if (size > 1)
-{
-Bubble_sort(0,size - 1,string);
-}
-}
-int main() 
-{
-    Creat ArrayFrequency[255];
-	char string[255];
-    int size,N_repeated;
-    scanf("%[^\n]",string);
-    size = strlen(string);
-	Bubble_sort(0,size,string);
-    N_repeated = Comparison(0,0,0,0,size,string,ArrayFrequency);
-	Print(N_repeated,ArrayFrequency);
+
+    for(i = 0; i < 256; i++)
+    {
+    	printf("%d %d\n",i, string[i]);
+    }
 }
