deb418548ba49efbb7dcf98bbf78dd7cd3c360b7
Elygledson Js <47836992+Elygledson@users.noreply.github.com>
2019-08-16 16:56:05 -0300

    ArrayFrequency.c

--- Huffman/ArrayFrequency.c
+++ Huffman/ArrayFrequency.c
@@ -1,45 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-/*typedef struct Data_character
-{
-	char character[1000];
-	int frequency;
-}Creat;*/
-
-
-struct Node
-{  
-    unsigned char caracter;
-    int frequencia;
-    struct Node *proximo; 
-};
-
-
-struct Node *novo_no(struct Node *head, unsigned char c, int f)
-{
-    if(head == NULL)
-    {
-        struct Node *novo = (struct Node*)malloc(sizeof(struct Node));
-        novo->caracter = c;
-        novo->frequencia = f;
-        novo->proximo = NULL;
-        return novo;
-    }
-    else
-    {
-        struct Node *novo = (struct Node*)malloc(sizeof(struct Node));
-        novo->caracter = c;
-        novo->frequencia = f;
-	    /**
-	    FALTA
-	    COMPLETAR
-	    */
-        return novo;
-    }
-}
-void zerar_array_frequencia(unsigned char array_frequencia[])
+void zerar_array_frequencia(char array_frequencia[])
 {
 	int i;
 	for(i = 0; i < 256; i++)
@@ -53,14 +14,10 @@ int main()
     unsigned char caracter;
     char nome_arquivo[30];
     unsigned char string[256];
-    struct Node* head = NULL;
     int i = 0;
-
     zerar_array_frequencia(string);
-
     scanf("%s", nome_arquivo);
     file_input = fopen(nome_arquivo, "rb");
-
     if(file_input == NULL)
 	{
 		printf("ERRO AO ABRIR ARQUIVO: %s\n", nome_arquivo);
@@ -73,14 +30,9 @@ int main()
 			printf("%c", caracter);
 			string[caracter]++;
 		}
-
-        //novo_no(head, caracter, )
     }
-
     for(i = 0; i < 256; i++)
     {
     	printf("%d %d\n",i, string[i]);
     }
-
-    return 0;
 }
