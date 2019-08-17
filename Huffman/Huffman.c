67078154f2ca4b1db4a600e3828fb4c755c428b1
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-08-17 17:41:10 -0300

    Merge nos codes

--- Huffman/Huffman.c
+++ Huffman/Huffman.c
@@ -1,50 +1,162 @@
-//AINDA NAO TESTEI
-
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
+#include<string.h>
 
-struct HuffTree
-{
-	int frequencia; //frequencia do caractere no arquivo
-	void *c;		//chave passada
-	struct HuffTree *esq;	//filho a esq
-	struct HuffTree *dir;	//filho a dir
-};
+#define MAX 256
 
-struct HuffTree *init()
+typedef struct Tree
 {
-	struct HuffTree *novo_valor = (struct HuffTree*)malloc(sizeof(struct HuffTree));
-	novo_valor->c = NULL;
-	novo_valor->frequencia = 0;
-	novo_valor->esq = NULL;
-	novo_valor->dir =  NULL;
-	return novo_valor;
+	int Value_node;
+	char symbol;
+	struct Tree *left;
+	struct Tree *right;
+} HuffTree;
+
+typedef struct data
+{
+	int frequency;
+	char character;
+	struct data *next;
+} Nodes;
+
+typedef struct Array
+{
+	Nodes *table[MAX];
+} Hash;
+
+/*we use this function for creat the nodes*/
+Nodes *CreatNode(int data,char character)
+{
+	Nodes* node     = (Nodes*)malloc(sizeof(Nodes));
+	node->frequency = data;
+	node->character = character;
+	return node;
 }
 
-struct HuffTree *add_no_arvore(int frequencia, void* valor, struct HuffTree *esq, struct HuffTree *dir)
+/*Here in this function we're going creat a node with sum of the elements*/
+/*HuffTree *Creation(int Ffrequency,int Sfrequency,char character)
 {
-	struct HuffTree *novo_valor = (struct HuffTree*)malloc(sizeof(struct HuffTree));
-	novo_valor->c = valor;
-	novo_valor->frequencia = frequencia;
-	novo_valor->esq = esq;
-	novo_valor->dir =  dir;
-	return novo_valor;
+	HuffTree *Node   = (HuffTree*)malloc(sizeof(HuffTree));
+	Node->Value_node = Ffrequency + Sfrequency;
+	Node->symbol     =  '*';
+	Node->left       = Node->right = NULL;
+	return Node;
+}*/
+
+HuffTree *create_nodes_huff(Nodes *node)
+{
+	HuffTree *Node   = (HuffTree*)malloc(sizeof(HuffTree));
+	Node->Value_node = node->frequency;
+	Node->symbol     = node->character;
+	Node->left       = Node->right = NULL;
+	return Node;
 }
 
-int eh_folha(struct HuffTree *huffman)
+/*It's in this function where we creat a array of pointers*/
+Hash *CreatTable(int size_table)
 {
-	if((huffman != NULL) && (huffman->esq == NULL) && (huffman->dir == NULL))
-		return 1;
+	int i;
+	Hash *Table = (Hash*)malloc(sizeof(Hash));
+	for(i = 0;i < size_table;i++)
+	{
+		Table->table[i] = NULL;
+	}
+	return Table;
+}
+
+void print_hash_table(Hash *Hash,int size)
+{
+	int i;
+	for (i = 0;i < size;i++)
+	{
+		printf("%d %c\n",Hash->table[i]->frequency,Hash->table[i]->character);
+	}
+}
+
+void print_huff_tree(HuffTree *huff_node) {
+	if (huff_node == NULL) return;
+	printf("(");
+	if (huff_node->symbol == ' ') {
+		printf("+");
+	}
 	else
-		return 0;
+	{
+		printf("%d ", huff_node->Value_node);
+		printf("%c", huff_node->symbol);
+	}
+	if (huff_node->left == NULL) {
+		printf("()");
+	} else {
+		print_huff_tree(huff_node->left);
+	} if (huff_node->right == NULL) {
+		printf("()");
+	} else {
+		print_huff_tree(huff_node->right);
+	}
+	printf(")");
 }
 
-void print_huffman()
+int main()
 {
+	FILE *file_input; //variavel que guardará o arquivo de entrada
+	unsigned char caracter;
+	char nome_arquivo[30];
+	unsigned int *string = (unsigned int*)calloc(256,sizeof(unsigned int));
+	int i = 0;
+	int cont = 0;
+	scanf("%[^\n]", nome_arquivo);
+	file_input = fopen(nome_arquivo, "rb");
+	int tam_hash = 0;
+	int j = 0;
+	int *indice = (int*)malloc(256 * sizeof(int));
 
-}
+	if(file_input == NULL)
+	{
+		printf("ERRO AO ABRIR ARQUIVO: %s\n", nome_arquivo);
+		return 0;
+	}
+	else
+	{
+		while(fscanf(file_input, "%c", &caracter) != EOF)
+		{
+			//printf("%c", caracter);
+			string[caracter]++;
+		}
+	}
 
-int main() {
+	for(i = 0; i < 256; i++)
+	{
+		if(string[i] >= 1)
+		{
+		//	printf("Characters: %c Frequency: %d\n",i, string[i]);
+			tam_hash++;
+			indice[j] = i;
+			j++;
+		}
+		/*Nodes* node = CreatNode(string[i], 'i');
+		Heap->table[i] = node;*/
+	}
+	//printf("%d\n", tam_heap);
+	Hash *hash = CreatTable(tam_hash);
 
-	return 0;
+	for(i = 0; i < tam_hash; i++)
+	{
+		Nodes* node = CreatNode(string[indice[i]], indice[i]);
+		hash->table[i] = node;
+		HuffTree *Node_h = create_nodes_huff(hash->table[i]);
+		print_huff_tree(Node_h);
+		//printf("Characters: %c Frequency: %d\n",indice[i], string[indice[i]]);
+		//printf("%d\n", indice[i]);
+	}
+	printf("\n");
+    //HuffTree *Node  = Creation(5,5);
+    //HuffTree *Node1 = Creation(5,20);
+    //Testes//
+    /*Nodes* node = CreatNode(2,'A');
+    Nodes* node2 = CreatNode(5,'B');
+    Heap->table[0] = node;
+    Heap->table[1] = node2;
+    print_heap_table(Heap,2);*/
+	//print_hash_table(hash,tam_hash);
 }
