9bf76bd1c62c5d2a42e261d58cd8755231d3e490
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-09-05 21:01:18 -0300

    Edição de algumas funcoes

--- Lista vs ABB/main.c
+++ Lista vs ABB/main.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_ELEMENT 1000
+#define MAX_ELEMENT 10000
 
 /*ESTRUTURA PARA LISTA*/
-struct Nos
-{
+struct Nos {
 	int valor;
 	struct Nos* proximo;
 };
 
 /*ESTRUTURA PARA ARVORE DE BUSCA BINÁRIA*/
-struct abb
-{
+struct abb {
 	int valor;
+	int altura;
 	struct abb *esq, *dir;
 };
 /*FUNCOES PROTOTIPAS PARA A LISTA ENCADEADA ORDENADA */
@@ -27,283 +26,236 @@ void troca(struct Nos *n1, struct Nos *n2);
 struct abb *novo_no(int valor);
 void printf_abb(struct abb *raiz);
 struct abb *inserir_na_abb(struct abb *novo, int valor);
-int buscar_na_arvore(struct abb *raiz, int valor, int comparacacao);
+struct abb *buscar_na_arvore(struct abb *raiz, int item, int *cont);
 int busca_na_lista(struct Nos* LISTA, int valor);
 void print_arvore(struct abb* raiz);
 void print_pre_ordem(struct abb* raiz);
 int numero_procurado_lista(struct Nos *LISTA, int valor);
 int numero_procurado_abb(struct abb *raiz, int valor);
+void selection_sort_vetor(int num[], int tam);
+struct abb *create_binary_tree(int item, struct abb *left, struct abb *right);
 //struct abb *buscar_na_arvore(int valor);
 
-int main()
-{
+int main() {
 	struct Nos *LISTA = NULL;
 	struct abb *raiz = NULL;
-	int i, n, escolha, valor_desejado;
+	int n, escolha, valor_desejado;
 	int *r_comp_lista;
 	int *r_comp_abb;
 	int *numeros_comparados;
-	int aux = 0;
-
-	r_comp_lista = (int*)malloc(sizeof(int) * MAX_ELEMENT);
-	r_comp_abb = (int*)malloc(sizeof(int) * MAX_ELEMENT);
-	numeros_comparados = (int*)malloc(sizeof(int) * MAX_ELEMENT);
+	long long int aux = 0;
+	long long int i;
+	int QTD = 0;
+	int *elementos = (int*)malloc(sizeof(int) * MAX_ELEMENT);
+	r_comp_lista = (int*) malloc(sizeof(int) * MAX_ELEMENT);
+	r_comp_abb = (int*) malloc(sizeof(int) * MAX_ELEMENT);
+	numeros_comparados = (int*) malloc(sizeof(int) * MAX_ELEMENT);
 	srand(time(NULL));
 
-	for(i = 0; i <= MAX_ELEMENT; i++)
-	{
-		n =(rand() % 1000);
-		push(&LISTA, n);
-		raiz = inserir_na_abb(raiz, n);
-		selectionSort(LISTA);
-	}
-		do
-		{
-			printf("******************************************\n");
-			printf("ESCOLHA UMA OPÇÃO:\n");
-			printf("1 - PROCURAR POR UM ELEMENTO\n");
-			printf("2 - IMPRIMIR A LISTA ENCADEADA ORDENADA\n");
-			printf("3 - IMPRIMIR A ABB\n");
-			printf("0 - SAIR\n");
-			printf("******************************************\n");
+	do {
+		printf("******************************************\n");
+		printf("ESCOLHA UMA OPÇÃO:\n");
+		printf("1 - GERAR ELEMENTOS\n");
+		printf("2 - IMPRIMIR A LISTA ENCADEADA ORDENADA\n");
+		printf("3 - IMPRIMIR A ABB\n");
+		printf("0 - SAIR\n");
+		printf("******************************************\n");
 
-			scanf("%d", &escolha);
+		scanf("%d", &escolha);
 
-			switch(escolha)
-			{
-				case 0:
-					break;
-				case 1:
-					for(i = 0; i < 100; i++)
-					{
-						n =(rand() % 1000);
-						if( ((numero_procurado_lista(LISTA, n)) && ((numero_procurado_abb(raiz, n))  == n)))
-						{
-							printf("Número procurado: %d\n", n);
-							//printf(">>>> %d <<<<\n", numero_procurado_abb(raiz, n));
-							printf("Número de comparações pra achar na lista: %d\n", busca_na_lista(LISTA, n));
-							r_comp_lista[aux] = busca_na_lista(LISTA, n);
-							printf("Número de comparações pra achar na abb: %d\n", buscar_na_arvore(raiz, n, 0));
-							r_comp_abb[aux] = buscar_na_arvore(raiz, n, 0);
-							numeros_comparados[aux] = n;
-							aux++;
-						}
-					}
-					printf("Números usados: ");
-					for(i = 0; i < aux; i++)
-					{
-						printf("%d ", numeros_comparados[i]);
-					}
-					printf("\n");
-					printf("\n");
-					printf("Comparações da lista: ");
-					for(i = 0; i < aux; i++)
-					{
-						printf("%d ", r_comp_lista[i]);
-					}
-					printf("\n");
-					printf("\n");
-					printf("Comparações da abb: ");
-					for(i = 0; i < aux; i++)
-					{
-						printf("%d ", r_comp_abb[i]);
-					}
-					printf("\n");
-					break;
-				case 2:
-					printf_lista(LISTA);
-					break;
-				case 3:
-					//printf_abb(raiz);
-					//print_arvore(raiz);
-					print_pre_ordem(raiz);
-					printf("\n");
-					break;
-				default:
-					printf("OPÇÃO INVALIDA!\n");
-					break;
+		switch (escolha) {
+		case 0:
+			break;
+		case 1:
+			printf("QUANTIDADE DE NUMEROS A SEREM GERADOS?\n");
+			scanf("%d", &QTD);
+			for (i = 0; i <= QTD; i++) {
+				n = (rand() % QTD);
+				elementos[i] = n;
+				push(&LISTA, n);
+				selectionSort(LISTA);
+				raiz = inserir_na_abb(raiz, n);
 			}
+			FILE *list_vs_abb = fopen("list_vs_abb_out.txt", "w");
+			fprintf(list_vs_abb, "value list abb\n");
+			for (i = 0; i < QTD; i++) {
+				int sorteado = elementos[i];
+				int comp_list = busca_na_lista(LISTA, sorteado);
+				int comp_abb;
+				//printf("Número sorteado: %d\n", sorteado);
+				//printf("Número de comparações: %d\n", comp_list);
+				buscar_na_arvore(raiz, sorteado, &comp_abb);
+				//fprintf(list_vs_abb, "%d\n", comp_abb[0]);
+				//printf("numero de comparacoes abb: %d\n", comp_abb);
+				fprintf(list_vs_abb, "%d %d %d\n", sorteado, comp_list, comp_abb);
+				comp_list =0;
+				comp_abb = 0;
+			}
+			fclose(list_vs_abb);
+			printf("Arquivo de saída gerado com sucesso!\n");
+			break;
+		case 2:
+			printf_lista(LISTA);
+			break;
+		case 3:
+			//printf_abb(raiz);
+			//print_arvore(raiz);
+			print_pre_ordem(raiz);
+			printf("\n");
+			break;
+		default:
+			printf("OPÇÃO INVALIDA!\n");
+			break;
+		}
 
-		} while(escolha);
+	} while (escolha);
 
 	return 0;
 }
 
-struct Nos *push(struct Nos **lista, int valor)
-{
-	struct Nos* init = (struct Nos*)malloc(sizeof(struct Nos));
+struct Nos *push(struct Nos **lista, int valor) {
+	struct Nos* init = (struct Nos*) malloc(sizeof(struct Nos));
 	init->valor = valor;
 	init->proximo = *lista;
 	*lista = init;
 }
 
-void selectionSort(struct Nos* LISTA)
+struct abb *create_binary_tree(int item, struct abb *left, struct abb *right) {
+	struct abb *new_bt = (struct abb*) malloc(sizeof(struct abb));
+	new_bt->valor = item;
+	new_bt->altura = 1;
+	new_bt->esq = new_bt->dir = NULL;
+	return new_bt;
+}
+void selection_sort_vetor(int num[], int tam)
 {
-    struct Nos* aux = LISTA;
-
-    while (aux) {
-        struct Nos* old = aux;
-        struct Nos* resltado = aux->proximo;
-
-        while (resltado) {
-            if (old->valor > resltado->valor)
-                old = resltado;
-
-            resltado = resltado->proximo;
-        }
-
-        troca(aux, old);
-        aux = aux->proximo;
+  int i, j, min, swap;
+  for (i = 0; i > (tam-1); i++)
+   {
+    min = i;
+    for (j = (i+1); j > tam; j++) {
+      if(num[j] > num[min]) {
+        min = j;
+      }
     }
+    if (i != min) {
+      swap = num[i];
+      num[i] = num[min];
+      num[min] = swap;
+    }
+  }
 }
 
-void printf_lista(struct Nos *lista)
-{
-	if(lista->proximo == NULL)
-	{
-		printf("A lista está vazia");
+void selectionSort(struct Nos* LISTA) {
+	struct Nos* aux = LISTA;
+
+	while (aux) {
+		struct Nos* old = aux;
+		struct Nos* resltado = aux->proximo;
+
+		while (resltado) {
+			if (old->valor > resltado->valor)
+				old = resltado;
+
+			resltado = resltado->proximo;
+		}
+
+		troca(aux, old);
+		aux = aux->proximo;
 	}
-	else
-	{
-		while(lista != NULL)
-		{
+}
+
+void printf_lista(struct Nos *lista) {
+	if (lista->proximo == NULL) {
+		printf("A lista está vazia");
+	} else {
+		while (lista != NULL) {
 			printf("%d\n", lista->valor);
 			lista = lista->proximo;
 		}
 	}
 }
 
-void troca(struct Nos *n1, struct Nos *n2)
-{
+void troca(struct Nos *n1, struct Nos *n2) {
 	int aux = n1->valor;
 	n1->valor = n2->valor;
 	n2->valor = aux;
 }
 
-void printf_abb(struct abb *raiz)
-{
-	if(raiz != NULL)
-	{
+void printf_abb(struct abb *raiz) {
+	if (raiz != NULL) {
 		printf_abb(raiz->esq);
 		printf("%d \n", raiz->valor);
 		printf_abb(raiz->dir);
 	}
 }
 
-void print_arvore(struct abb* raiz)
-{
-	if(raiz != NULL)
-	{
+void print_arvore(struct abb* raiz) {
+	if (raiz != NULL) {
 		print_arvore(raiz->esq);
 		printf("\n%d", raiz->valor);
 		print_arvore(raiz->dir);
 	}
 }
 
-void print_pre_ordem(struct abb* raiz)
+void print_pre_ordem(struct abb* raiz) {
+	if (raiz != NULL) {
+		printf("\n%d", raiz->valor);
+		print_pre_ordem(raiz->esq);
+		print_pre_ordem(raiz->dir);
+	}
+}
+
+struct abb *inserir_na_abb(struct abb *raiz, int valor)
 {
-    if(raiz != NULL)
-    {
-        printf("\n%d", raiz->valor);
-        print_pre_ordem(raiz->esq);
-        print_pre_ordem(raiz->dir);
+	/* 1.  Perform the normal BST rotation */
+	if (raiz == NULL) {
+		return (create_binary_tree(valor, NULL, NULL));
+	}
+
+	struct abb *aux = raiz;
+	struct abb *previous;
+
+	while (aux != NULL) {
+		previous = aux;
+		if (valor < aux->valor) {
+			aux = aux->esq;
+		} else {
+			aux = aux->dir;
+		}
+	}
+
+	if (valor < previous->valor) {
+		previous->esq = create_binary_tree(valor, NULL, NULL);
+	} else {
+		previous->dir = create_binary_tree(valor, NULL, NULL);
+	}
+
+	/* return the (unchanged) bt pointer */
+	return raiz;
+}
+
+struct abb *buscar_na_arvore(struct abb *raiz, int item, int *cont) {
+	struct abb  *new_bt = raiz;
+    *cont += 1;
+    if ((new_bt == NULL) || (new_bt->valor == item)) {
+        return new_bt;
+    } else if (new_bt->valor > item) {
+    	buscar_na_arvore(new_bt->esq, item, cont);
+    } else {
+    	buscar_na_arvore(new_bt->dir, item, cont);
     }
 }
 
-struct abb *inserir_na_abb(struct abb *novo, int valor)
-{
-	if(novo == NULL)
-	{
-		struct abb *new = (struct abb*)malloc(sizeof(struct abb));
-		new->valor = valor;
-		new->esq = NULL;
-		new->dir = NULL;
-
-		return new;
-	}
-	else
-	{
-		if(valor >= novo->valor)
-		{
-			novo->dir = inserir_na_abb(novo->dir, valor);
-		}
-		else if(valor < novo->valor)
-		{
-			novo->esq = inserir_na_abb(novo->esq, valor);
-		}
-	}
-	return novo;
-}
-
-int buscar_na_arvore(struct abb *raiz, int valor, int comparacacao)
-{
-	if(raiz == NULL)
-	{
-		return 0;
-	}
-	else
-	{
-		if(valor == raiz->valor)
-		{
-			//printf("Número de comparações pra achar na abb: %d\n", comparacacao);
-			return comparacacao;
-		}
-		if(valor >= raiz->valor)
-		{
-			return buscar_na_arvore(raiz->dir, valor, ++comparacacao);
-		}
-		if(valor < raiz->valor)
-		{
-			return buscar_na_arvore(raiz->esq,valor, ++comparacacao);
-		}
-	}
-}
-
-int busca_na_lista(struct Nos* LISTA, int valor)
-{
+int busca_na_lista(struct Nos* LISTA, int valor) {
 	int comparacao = 0;
-	while(LISTA != NULL)
-	{
-		if(LISTA->valor == valor)
-		{
+	while (LISTA != NULL) {
+		if (LISTA->valor == valor) {
+			comparacao++;
 			//printf("Número de comparações pra achar na lista: %d\n", comparacao);
 			return comparacao;
 		}
-		LISTA = LISTA->proximo;
 		comparacao++;
-	}
-}
-
-int numero_procurado_lista(struct Nos *LISTA, int valor)
-{
-	while(LISTA != NULL)
-	{
-		if(LISTA->valor == valor)
-		{
-			return 1;
-			break;
-		}
 		LISTA = LISTA->proximo;
-	}
-	return 0;
-}
-
-int numero_procurado_abb(struct abb *raiz, int valor)
-{
-	if(raiz == NULL)
-	{
-		return 0;
-	}
-	if(raiz->valor == valor)
-	{
-		return (valor);
-	}
-	if(raiz->valor > valor)
-	{
-		return (numero_procurado_abb(raiz->esq, valor));
-	}
-	else
-	{
-		return (numero_procurado_abb(raiz->dir, valor));
 	}
 }
