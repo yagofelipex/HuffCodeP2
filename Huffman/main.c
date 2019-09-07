69c0ac097bbad2e865a5234cf96814aa82c226fc
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-09-07 15:44:25 -0300

    Inserindo uma explicação sobre

--- Huffman/main.c
+++ Huffman/main.c
@@ -1,3 +1,16 @@
+
+/* Este programa comprime ou extrai arquivos usando a Codificação de Huffman.
+ * Na compressão, o arquivo de saída terá nos 3 primeiros bits o número cor-
+ * respondente à quantidade de bits de lixo no final do arquivo. Nos 13 bits
+ * seguintes haverá o número correspondente à quantidade de bytes ocupada pe-
+ * la árvore de Huffman. A árvore de Huffman será escrita após os dois primei-
+ * ros bytes em pré-ordem, em codificação ASCII. Após a árvore de Huffman, ha-
+ * verá os bits com os dados do arquivo comprimido usando a árvore impressa
+ * como codificação. Na descompressão, as regras descritas anteriormente devem
+ * ter sido usadas para comprimir o arquivo de entrada.
+ * Este é o arquivo principal do projeto, que deve ser compilado utilizando essa classe.
+ */
+
 #include "Descompress.c"
 #include "Compress.c"
 #include <stdio.h>
